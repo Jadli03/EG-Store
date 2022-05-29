8dcbbbe0aeea8de104814698c499731221606176
Suraj Jadli <surajjadli@yahoo.com>
2022-05-29 11:32:50 +0530

    pushing EG-Store project

---
+++ frontend/src/components/Meta.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='keyword' content={keywords} />
+    </Helmet>
+  )
+}
+
+Meta.defaultProps = {
+  title: 'Welcome To EG-Store',
+  description: 'We sell all esstenial products',
+  keywords: 'Rice, Wheat and all esstenial products',
+}
+
+export default Meta
