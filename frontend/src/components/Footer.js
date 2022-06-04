f4e60d5539ea42dccf05020f4f1a663c218e5b12
Suraj Jadli <surajjadli@yahoo.com>
2022-06-04 17:50:11 +0530

    changing header name

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; EG-Store</Col>
+          <Col className='text-center py-3'>Copyright &copy; Global Gadgets</Col>
         </Row>
       </Container>
     </footer>
